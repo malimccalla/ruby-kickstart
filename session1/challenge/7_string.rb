0a3c0c535aacad7a5653035dd505f682cb80d60b
Mali Michael <malimccalla@gmail.com>
2016-06-26 07:03:38 +0100

    Complete 1:7

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,15 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  new_string = ''
+  string.chars.each_with_index do |letter,index|
+    if letter == "r" || letter == "R"
+      new_string << string[index+1] unless string[index+1].nil?
+    end
+  end
+  new_string
 end
